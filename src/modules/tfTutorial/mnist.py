74480b4a1f9d94f472ce6117efbdb108c02511ab
srinesh <csrinesh@gmail.com>
2020-01-08 19:12:03 +0800

    ran the test model

--- src/modules/tfTutorial/mnist.py
+++ src/modules/tfTutorial/mnist.py
@@ -1,6 +1,10 @@
 import tensorflow as tf 
 from logs import logDecorator  as lD
 import jsonref
+import requests
+requests.packages.urllib3.disable_warnings()
+import ssl
+import os
 
 config = jsonref.load(open('../config/config.json'))
 logBase = config['logging']['logBase'] + '.modules.module1.module1'
@@ -22,6 +26,38 @@ def doSomething(logger):
 
     return
 
+@lD.log(logBase + '.samplecode')
+def samplecode(logger):
+    '''print a line
+    
+    This function simply prints a single line
+    
+    Parameters
+    ----------
+    logger : {logging.Logger}
+        The logger used for logging error information
+    '''
+
+    mnist = tf.keras.datasets.mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28,28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10, activation='softmax')]
+    )
+
+    model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=5)
+
+    model.evaluate(x_test,  y_test, verbose=2)          
+    return
+
 @lD.log(logBase + '.main')
 def main(logger, resultsDict):
     '''main function for module1
@@ -40,6 +76,6 @@ def main(logger, resultsDict):
         overwriting command line arguments as needed.
     '''
 
-    doSomething()
+    samplecode()
 
     return
